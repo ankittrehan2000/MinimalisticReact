b54e4d75d5731f8b6edfd42003ee482c5f2d2662
ankittrehan2000 <47463734+ankittrehan2000@users.noreply.github.com>
2020-02-28 15:26:25 -0500

    Pushing required js files

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,1 +1,59 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/About';
+import ContactPage from './pages/Contact';
 
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      title: "Ankit",
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'}
+      ],
+      home: {
+        title: 'Coder',
+        subTitle: 'Learner',
+        description: 'Look at my projects'
+      },
+      About: {
+        title: 'About Me',
+      },
+      Contact: {
+        title: 'Let\'s talk',
+      }
+    }
+  }
+  render(){
+  return (
+    <Router>
+      <Container className="p-0" fluid={true}>
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
+          <Navbar.Brand>Ankit Trehan</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-toggle" />
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Route path="/" exact render={() => <HomePage title={this.state.home.title}  subTitle={this.state.home.subTitle} text={this.state.home.description}/>}></Route>
+        <Route path="/about" exact render={() => <AboutPage title={this.state.About.title}/>}></Route>
+        <Route path="/contact" exact render={() => <ContactPage title={this.state.Contact.title}/>}></Route>
+        <Footer />
+      </Container>
+    </Router>
+  );
+}
+}
+export default App;
